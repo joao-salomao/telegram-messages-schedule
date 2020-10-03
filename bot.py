7d3329d788090dacba58b52fe14dab0284f48758
João Salomão <joaosalomao.613@gmail.com>
2020-10-03 03:52:31 -0300

    Using telegram library to create a job queue

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
-import time
 import logging
-import schedule
 from decouple import config
 from telegram.ext import Updater
 from telegram.ext import Filters
@@ -21,38 +19,38 @@ logger.setLevel(logging.DEBUG)
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
+job_queue = updater.job_queue
 
 # Create repositories
 group_repository = GroupRepository()
 message_repository = MessageRepository()
 group_message_repository = GroupMessageRepository()
 
+
 def new_member(update, context):
     for member in update.message.new_chat_members:
         if member.username == BOT_NAME:
-            group_repository.create(update.message.chat.title, update.message.chat.id)
+            group_repository.create(
+                update.message.chat.title, update.message.chat.id)
 
 
-def send_scheduled_messages():
-    # TODO
-    # Ex: dispatcher.bot.send_message(chat_id='-441809317', text='Hi there', disable_web_page_preview=False)
-    pass
+def send_scheduled_messages(context):
+    scheduled_messages = group_message_repository.get_scheduled_messages_to_current_time()
+    for scheduled_message in scheduled_messages:
+        context.bot.send_message(chat_id=scheduled_message.group.telegram_id,
+                                    text=scheduled_message.message.link, disable_web_page_preview=False)
 
-### Setup schedule
+
+# - Setup job queue
 # Sends scheduled messages to the current time
-schedule.every().minutes.do(send_scheduled_messages)
+job_queue.run_repeating(send_scheduled_messages, interval=60, first=0)
 
-### Setup handlers
-# When the bot is added to any group, the 
-# group must be registered in the database 
-dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_member))
+# - Setup handlers
+# When the bot is added to any group, the
+# group must be registered in the database
+dispatcher.add_handler(MessageHandler(
+    Filters.status_update.new_chat_members, new_member))
 
-# Watch telegram updates
+# - Watch telegram updates
 updater.start_polling()
 updater.idle()
-
-# Start schedule task
-while True:
-    schedule.run_pending()
-    time.sleep(60)
-
